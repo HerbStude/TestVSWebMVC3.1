bda648a66bc739d9c0470fbc8c8e88548f00a748
ryclos <loogle@hotmail.com>
2020-04-23 13:21:05 -1000

    Initial commit

---
+++ jeudontonestlehero.Core.Data/Models/AventureViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jeudontonestlehero.Core.Data
+{
+    public class AventureViewModel
+    {
+        #region Propriété
+        /// <summary>
+        /// C'est l'id de chaque aventure
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// C'est le titre de chaque aventure
+        /// </summary>
+        public string Titre { get; set; }
+        #endregion
+    }
+}
---
+++ jeudontonestleheros.Web.UI/Controllers/AventureController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using jeudontonestlehero.Core.Data;
+using jeudontonestleheros.Web.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace jeudontonestleheros.Web.UI.Controllers
+{
+    public class AventureController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            ViewBag.MonTitre = "Aventures";
+
+
+            #region Nouvelle Méthode List<>
+            List<AventureViewModel> maList = new List<AventureViewModel>
+            {
+                new AventureViewModel()
+                {
+                    Id = 1,
+                    Titre = "Ma première aventure"
+                },
+
+                new AventureViewModel()
+                {
+                    Id = 2,
+                    Titre = "Ma seconde aventure"
+                }
+            };
+            #endregion
+
+            #region Ancienne méthode List<>
+            /*
+            List<AventureViewModel> maList = new List<AventureViewModel>();
+
+            maList.Add(new AventureViewModel()
+            {
+                Id = 1,
+                Titre = "Ma première aventure"
+            });
+
+            maList.Add(new AventureViewModel()
+            {
+                Id = 2,
+                Titre = "Ma seconde aventure"
+            });*/
+            #endregion
+
+            return View(maList);
+        }
+    }
+}
